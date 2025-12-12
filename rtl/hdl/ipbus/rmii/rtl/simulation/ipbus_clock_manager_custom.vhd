82712ab61a86280a4ffcfccf8bd9f11f17dde11d
Ludovic Blanc <ludovic.blanc@epfl.ch>
2025-12-12 16:59:51 +0100

    add missing license header

--- rtl/hdl/ipbus/rmii/rtl/simulation/ipbus_clock_manager_custom.vhd
+++ rtl/hdl/ipbus/rmii/rtl/simulation/ipbus_clock_manager_custom.vhd
@@ -3,7 +3,7 @@
 -- EPFL - TCL 
 
 ---------------------------------------------------------------------------------
---
+--	from:
 --   Copyright 2017 - Rutherford Appleton Laboratory and University of Bristol
 --
 --   Licensed under the Apache License, Version 2.0 (the "License");
